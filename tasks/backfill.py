0fb48f410c46834db4bc7ef3f843eb1b3f73ca1e
Lim Zhi Chao <zhichao@Lims-MacBook-Pro-2.local>
2025-11-24 11:09:23 +0800

    Updated backfill.py

--- tasks/backfill.py
+++ tasks/backfill.py
@@ -14,7 +14,8 @@ async def backfill(
         "filt_rec",
         "filt_hist",
         "model_data_add",
-        "model_data_onset"
+        "model_data_onset",
+        "model_data_hist"
     ]
 
 
@@ -28,18 +29,16 @@ async def backfill(
 
     print("Documents deleted from all collections")
 
-    for i in ['sql', 'raw']:
+    for c in ['sql_hist', 'raw_hist', 'raw_rec']:
 
-        for j in ['rec', 'hist']:
+        watermark_log = {
+            'pipeline_name' : c,
+            'last_utime' : '2000-01-01 00:00:00',
+        }
 
-            watermark_log = {
-                'pipeline_name' : f'{i}_{j}',
-                'last_utime' : '2000-01-01 00:00:00',
-            }
-
-            await mongo.upsert_documents_hashed(
-                [watermark_log],
-                coll_name='watermarks'
-            )
+        await mongo.upsert_documents_hashed(
+            [watermark_log],
+            coll_name='watermarks'
+        )
 
     print("Watermarks updated for all colections")
