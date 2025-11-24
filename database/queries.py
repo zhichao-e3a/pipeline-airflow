3afad4c03afc079068b88737f402c49d9d5895c6
Lim Zhi Chao <zhichao@Lims-MacBook-Pro-2.local>
2025-11-24 15:02:19 +0800

    Standardised queries

--- database/queries.py
+++ database/queries.py
@@ -50,3 +50,67 @@ AND uu.mobile IN ({numbers})
 AND r.start_ts BETWEEN UNIX_TIMESTAMP({start}) AND UNIX_TIMESTAMP({end})
 ;
 """
+
+RECRUITED_PATIENTS_QUERY = """
+SELECT
+u.mobile,
+FROM_UNIXTIME(r.start_ts) AS m_time,
+r.basic_info,
+r.conclusion
+FROM extant_future_user.user AS u
+JOIN extant_future_data.origin_data_record AS r
+ON u.id = r.user_id
+WHERE
+u.mobile
+IN
+({mobile_query_str})
+;
+"""
+
+HISTORICAL_PATIENTS_QUERY = """
+SELECT
+uu.name,
+u.mobile,
+uu.age,
+oo.earliest,
+oo.latest,
+oo.basic_info,
+oo.conclusion,
+uu.height,
+uu.old_weight,
+uu.expected_born_date AS edd,
+mm.record_type,
+mm.record_answer
+FROM extant_future_user.user AS u
+JOIN extant_future_user.user_detail AS uu ON u.id = uu.uid
+JOIN
+(
+	SELECT
+	o1.user_id,
+	FROM_UNIXTIME(o1.earliest) AS earliest,
+	FROM_UNIXTIME(o1.latest) AS latest,
+	o2.basic_info,
+	o2.conclusion
+	FROM
+	(
+		SELECT
+		user_id,
+		MIN(start_ts) AS earliest,
+		MAX(start_ts) AS latest
+		FROM extant_future_data.origin_data_record
+		GROUP BY user_id
+	) AS o1
+	JOIN
+	(
+		SELECT user_id, start_ts, basic_info, conclusion
+		FROM extant_future_data.origin_data_record
+	) AS o2
+	ON o1.user_id = o2.user_id AND o1.earliest = o2.start_ts
+) AS oo ON uu.uid = oo.user_id
+LEFT JOIN extant_future_user.medical_record AS mm ON oo.user_id = mm.user_id AND mm.record_type IN (1, 2, 4, 5, 8, 13)
+WHERE
+u.mobile
+IN
+({mobile_query_str})
+;
+"""
