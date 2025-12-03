1929eae13c0f849bb8b7018e2d6d52d8295bc877
Lim Zhi Chao <zhichao@Lims-MacBook-Pro-2.local>
2025-12-03 18:09:55 +0800

    Updated backfill print statements

--- tasks/backfill.py
+++ tasks/backfill.py
@@ -19,21 +19,19 @@ async def backfill(
     ]
 
 
-    for c in collections_to_del:
+    for _, c in enumerate(collections_to_del):
 
         n_del = await mongo.delete_all_documents(
             coll_name=c
         )
 
-        print(f"{c}: {n_del} documents deleted")
-
-    print("Documents deleted from all collections")
+        print(f"[{_+1}][{c}] {n_del} documents deleted")
 
     for c in ['sql_hist', 'raw_hist', 'raw_rec']:
 
         watermark_log = {
             'pipeline_name' : c,
-            'last_utime' : '2000-01-01 00:00:00',
+            'last_utime'    : '2000-01-01 00:00:00',
         }
 
         await mongo.upsert_documents_hashed(
